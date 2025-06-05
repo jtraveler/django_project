b47c0a3a4b3fb5d270baa9da671aa9559986dee1
jtraveler <matthew.jtraveler@gmail.com>
2025-06-05 11:07:03 +0200

    django progress

---
+++ hello_world/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    if request.method == "GET":
+        return HttpResponse("This was a GET request")
+    elif request.method == "POST":
+        return HttpResponse(request.method)
